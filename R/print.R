8d34cb305bf0b4d86d84875fcd493a0a417243d6
Reece-Milner <94184617+Reece-Milner@users.noreply.github.com>
2023-04-17 16:20:54 -0500

    various updates

--- R/print.R
+++ R/print.R
@@ -1,15 +1,17 @@
 #' @title print
 #'
 #' @param x Object to print
+#' @param ... Extra inputs
+#' @importFrom stats t.test
+#' @import knitr
 #'
 #' @return The data frame of the Rttest object
-#'
-#' @export print.Rttest
+#' @export
 #'
 #' @examples
 #' \dontrun{print(obj)}
-print.Rttest <- function(x) {
-  library(kableExtra)
+print.Rttest <- function(x, ...) {
+  #library(kableExtra)
   knitr::kable(x[[1]], format = "markdown")
 }
 
